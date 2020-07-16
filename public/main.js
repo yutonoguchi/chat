f6eaf1e0a5a25d976b89ad21a4c9e217d292e9d4
野口弘一 <hirokazu2002@noguchikouichinoMacBook-Air.local>
2020-07-16 18:13:56 +0900

    色々変更した

--- public/main.js
+++ public/main.js
@@ -8,25 +8,64 @@ const name = document.getElementById("name");
 
 let username='';
 nameform.addEventListener('submit', function(event){
-  username = name.value;
+  if(name.value!==''){
+    username = name.value;
+    nameform.style.display ="none";
+    form.style.display ="block";
+
+    socketio.emit('signin');
+
+    const msg = {msg: username + ' さんが参加しました。', name: 'システム'};
+    socketio.emit('message', msg);
+  }
+
   event.preventDefault();
-  nameform.style.display ="none";
-  form.style.display ="block";
 })
 
 form.addEventListener('submit', function(event){
-  const msg = JSON.stringify({msg: input.value, name: username})
-  socketio.emit('message', msg);
-  input.value='';
+  if(input.value!==''){
+    const msg = {msg: input.value, name: username};
+    socketio.emit('message', msg);
+    input.value='';
+  }
   event.preventDefault();
 })
-socketio.on('message',function(msg){
-  const obj = JSON.parse(msg);
 
+socketio.on('message',function(msg){
+  if(username===''){
+    // まだ参加していなかったら、チャットを表示しない
+    return;
+  }
+  displayMessage(msg);
+});
+
+// 参加時に過去のメッセージを受け取る
+socketio.on('signin',function(msgs){
+  for(let i=0;i<msgs.length;i++){
+    const msg = msgs[i];
+    displayMessage(msg);
+  }
+});
+
+function displayMessage(msg){
   const dt = document.createElement("dt");
   const dd = document.createElement("dd");
-  dt.append(obj.name);
+  const dd2 = document.createElement("dd");
+  dt.append(msg.name);
   chats.append(dt);
-  dd.append(obj.msg);
+  dd.append(msg.msg);
   chats.append(dd);
-});
+  dd2.append(formatDate(new Date(msg.date), 'yyyy/MM/dd HH:mm:ss'));
+  chats.append(dd2);
+}
+
+function formatDate (date, format) {
+  format = format.replace(/yyyy/g, date.getFullYear());
+  format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
+  format = format.replace(/dd/g, ('0' + date.getDate()).slice(-2));
+  format = format.replace(/HH/g, ('0' + date.getHours()).slice(-2));
+  format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
+  format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
+  format = format.replace(/SSS/g, ('00' + date.getMilliseconds()).slice(-3));
+  return format;
+};
